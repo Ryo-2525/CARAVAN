714c2b9d32347c3d4194cd918bd777526d4a6b09
Ryo-2525 <seama.24.music@gmail.com>
2021-02-09 06:37:35 +0000

    [start]upload

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -16,6 +16,7 @@ class BlogsController < ApplicationController
 
   def create
     blog = Blog.new(blog_params)
+    binding.pry
     blog.save
     redirect_to blogs_path(blog.id)
   end
@@ -28,6 +29,11 @@ class BlogsController < ApplicationController
     blog = Blog.find(params[:id])
     blog.update(blog_params)
     redirect_to blog_path(blog)
+  end
+  def destroy
+    blog = Blog.find(params[:id])
+    blog.destroy
+    redirect_to blogs_path
   end
 
   private
